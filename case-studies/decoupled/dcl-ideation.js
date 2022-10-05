a9b4e2395183bad68ca37276e2840e94fcdcb05a
Martin Duggan <info@martinduggan.com>
2022-10-05 14:20:52 -0400

    dcl ideation content

--- case-studies/decoupled/dcl-ideation.js
+++ case-studies/decoupled/dcl-ideation.js
@@ -16,7 +16,9 @@ export default class DCLideation extends HTMLElement {
 	}
 	getModel() {
 		return new Promise((res, rej) => {
-			fetch("../../content/decoupledLive.json")
+			fetch(
+				"https://raw.githubusercontent.com/mdugg/folio22/main/content/decoupledLive.json"
+			)
 				.then((data) => data.json())
 				.then((json) => {
 					this.render(json);
